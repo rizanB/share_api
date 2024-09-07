810b9c83d2f9bc4d7ecc0d394f9d15edf23923a3
rizanb <rijann5@gmail.com>
2024-09-07 20:31:29 +0545

    chore: remove query for script in endpoint

--- app.py
+++ app.py
@@ -24,30 +24,36 @@ app = Flask(__name__)
 
 @app.route('/share_price', methods = ['GET'])
 def get_last_updated_price():
-
-        symbol = request.args.get('symbol').upper()
-
-        if not symbol:
-            return jsonify({"error": "Symbol parameter is required"}), 400
-
+       
+        
+        # symbol = request.args.get('symbol').upper()
+# 
+        # if not symbol:
+            # return jsonify({"error": "Symbol parameter is required"}), 400
+        
         if not os.path.exists(CSV_FILE_PATH):
             return jsonify({"error": "CSV file not found"}), 404
 
         df = pd.read_csv(CSV_FILE_PATH, on_bad_lines="skip")
+        # print(df)
 
-        stock_data = df[df['SYMBOL'] == symbol]
+        # stock_data = df
 
-        if stock_data.empty:
-            return jsonify({"error": "Symbol not found"}), 404
+        # if stock_data.empty:
+        #     return jsonify({"error": "Symbol not found"}), 404
 
-        last_updated_price = stock_data.iloc[0]['LAST_UPDATED_PRICE']
-        security_name = stock_data.iloc[0]['SECURITY_NAME']
+        # last_updated_price = stock_data.iloc[0]['LAST_UPDATED_PRICE']
+        # security_name = stock_data.iloc[0]['SECURITY_NAME']
 
-        return jsonify({
-            "symbol": symbol,
-            "security_name": security_name,
-            "last_updated_price": last_updated_price
-        })
+        # return jsonify({
+        #     "symbol": symbol,
+        #     "security_name": security_name,
+        #     "last_updated_price": last_updated_price
+        # })
+
+        df_as_dict = df.to_dict(orient='list')
+
+        return jsonify(df_as_dict)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
--- download_csv.py
+++ download_csv.py
@@ -17,6 +17,7 @@ def download_csv(url, dir):
     try:
         driver.get(url)
 
+        print('ready to wait')
         wait = WebDriverWait(driver, 20)
         download_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Download as CSV')]")))
         download_button.click()
